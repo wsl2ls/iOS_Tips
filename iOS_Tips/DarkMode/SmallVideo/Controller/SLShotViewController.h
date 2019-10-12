4238b36bf877337cf36f88964f45f70c81e3c6b0
wsl <15324956576@163.com>
2019-10-12 14:03:08 +0800

    调整文件目录

---
+++ iOS_Tips/DarkMode/SmallVideo/Controller/SLShotViewController.h
@@ -0,0 +1,18 @@
+//
+//  SLShotViewController.h
+//  DarkMode
+//
+//  Created by wsl on 2019/9/18.
+//  Copyright © 2019 wsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 拍摄视图控制器
+@interface SLShotViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
