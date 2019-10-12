c989406000b4408b64f8ccb8ce3e242a62dbe994
wsl <15324956576@163.com>
2019-10-12 14:03:33 +0800

    调整文件目录

---
+++ iOS_Tips/DarkMode/SmallVideo/View/SLEditMenuView.h
@@ -0,0 +1,29 @@
+//
+//  SLEditMenuView.h
+//  DarkMode
+//
+//  Created by wsl on 2019/10/9.
+//  Copyright © 2019 wsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+///视频和图片的编辑类型
+typedef NS_ENUM(NSUInteger, SLEditMenuType) {
+    /// 涂鸦
+    SLEditMenuTypeGraffiti = 0,
+    /// 文字
+    SLEditMenuTypeText,
+    /// 贴画
+    SLEditMenuTypeSticking,
+    /// 裁剪
+    SLEditMenuTypeCutting
+};
+
+/// 底部音视频编辑主菜单栏
+@interface SLEditMenuView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
