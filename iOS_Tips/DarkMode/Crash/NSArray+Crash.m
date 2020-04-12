58ddc2b2f0011a1b0a100676701bab459c33c14f
wsl <15324956576@163.com>
2020-04-12 23:26:16 +0800

    MutableArray crash防护

--- iOS_Tips/DarkMode/Crash/NSArray+Crash.m
+++ iOS_Tips/DarkMode/Crash/NSArray+Crash.m
@@ -23,11 +23,15 @@
     SL_ExchangeInstanceMethod(NSClassFromString(@"__NSPlaceholderArray"), @selector(initWithObjects:count:), NSClassFromString(@"__NSPlaceholderArray"), @selector(sl_initWithObjects:count:));
     
     
-    //
-    //替换可变数组方法
-    //        Method oldMutableObjectAtIndex = class_getInstanceMethod(objc_getClass("__NSArrayM"), @selector(objectAtIndex:));
-    //    Method newMutableObjectAtIndex =  class_getInstanceMethod(objc_getClass("__NSArrayM"), @selector(mutableObjectAtSafeIndex:));
-    //    method_exchangeImplementations(oldMutableObjectAtIndex, newMutableObjectAtIndex);
+    // 不可变数组
+    // 越界保护
+    SL_ExchangeInstanceMethod(NSClassFromString(@"__NSArrayM"), @selector(objectAtIndex:), NSClassFromString(@"__NSArrayM"), @selector(sl_mObjectAtIndex:));
+    SL_ExchangeInstanceMethod(NSClassFromString(@"__NSArrayM"), @selector(objectAtIndexedSubscript:), NSClassFromString(@"__NSArrayM"), @selector(sl_mObjectAtIndexedSubscript:));
+    SL_ExchangeInstanceMethod(NSClassFromString(@"__NSArrayM"), @selector(insertObject:atIndex:), NSClassFromString(@"__NSArrayM"), @selector(sl_insertObject:atIndex:));
+    SL_ExchangeInstanceMethod(NSClassFromString(@"__NSArrayM"), @selector(removeObjectsInRange:), NSClassFromString(@"__NSArrayM"), @selector(sl_removeObjectsInRange:));
+    
+    
+    
 }
 
 #pragma mark - Help Methods
@@ -50,7 +54,7 @@ void SL_ExchangeClassMethod(Class _class ,SEL _originalSel,SEL _exchangeSel){
 }
 
 
-#pragma mark - Exchange Methods
+#pragma mark - Array Safe Methods
 //[array objectAtIndex:0] 越界
 - (id)sl_objectAtIndex:(NSInteger)index {
     if (index >= self.count || !self.count) {
@@ -106,11 +110,68 @@ void SL_ExchangeClassMethod(Class _class ,SEL _originalSel,SEL _exchangeSel){
             index++;
         }else{
             //记录错误
-            NSString *errorInfo = [NSString stringWithFormat:@"异常:数组有nil值 *** -[__NSPlaceholderArray initWithObjects:count:]: attempt to insert nil object from objects[%d]",i];
+            NSString *errorInfo = [NSString stringWithFormat:@"异常:数组nil值 *** -[__NSPlaceholderArray initWithObjects:count:]: attempt to insert nil object from objects[%d]",i];
             NSLog(@"%@",errorInfo);
         }
     }
     return [self sl_initWithObjects:objectsNew count:index];
+}
+
+#pragma mark - MutableArray Safe Methods
+//越界
+- (id)sl_mObjectAtIndex:(NSInteger)index {
+    if (index >= self.count || !self.count) {
+        @try {
+            return [self sl_mObjectAtIndex:index];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"异常:数组越界 %@", exception.reason);
+            return nil;
+        }
+    }else {
+        return [self sl_mObjectAtIndex:index];
+    }
+}
+//越界
+- (id)sl_mObjectAtIndexedSubscript:(NSInteger)index {
+    if (index >= self.count || !self.count) {
+        //记录错误
+        //NSString *errorInfo = [NSString stringWithFormat:@"*** -[__NSArrayI objectAtIndexedSubscript:]: index %ld beyond bounds [0 .. %ld]'",(unsigned long)index,(unsigned long)self.count];
+        @try {
+            return [self sl_mObjectAtIndexedSubscript:index];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"异常:数组越界 %@", exception.reason);
+            return nil;
+        }
+    }
+    return [self sl_mObjectAtIndexedSubscript:index];
+}
+//越界
+- (void)sl_removeObjectsInRange:(NSRange)range {
+    if (range.location+range.length>self.count) {
+        NSString *errorInfo = [NSString stringWithFormat:@"异常:数组越界 *** -[__NSArrayM removeObjectsInRange:]: range {%ld, %ld} extends beyond bounds [0 .. %ld]",(unsigned long)range.location,(unsigned long)range.length,(unsigned long)self.count];
+        NSLog(@"%@",errorInfo);
+        return;
+    }
+    [self sl_removeObjectsInRange:range];
+}
+//越界 nil值
+- (void)sl_insertObject:(id)object atIndex:(NSInteger)index {
+    if (object == nil) {
+        NSLog(@"异常:数组nil值 ***  -[__NSArrayM insertObject:atIndex:]: object cannot be nil");
+        return;
+    }
+    if (index > self.count) {
+        @try {
+            return [self sl_insertObject:object atIndex:index];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"异常:数组越界 %@", exception.reason);
+        }
+    }else {
+        [self sl_insertObject:object atIndex:index];;
+    }
 }
 
 @end
--- iOS_Tips/DarkMode/Crash/SLCrashViewController.m
+++ iOS_Tips/DarkMode/Crash/SLCrashViewController.m
@@ -26,16 +26,17 @@
 - (void)setupUI {
     self.navigationItem.title = @"iOS Crash防护";
     
-    [self testArray];
+    [self testMutableArray];
 }
 
 #pragma mark - HelpMethods
-//数组防护 越界和nil值
+//不可变数组防护 越界和nil值
 - (void)testArray {
+    //越界
     NSArray *array = @[@"且行且珍惜"];
     id elem1 = array[3];
     id elem2 = [array objectAtIndex:2];
-    
+    //nil值
     NSString *nilStr = nil;
     NSArray *array1 = @[nilStr];
     NSString *strings[2];
@@ -43,6 +44,18 @@
     strings[1] = nilStr;
     NSArray *array2 = [NSArray arrayWithObjects:strings count:2];
     NSArray *array3 = [NSArray arrayWithObject:nil];
+}
+
+//可变数组防护 越界和nil值
+- (void)testMutableArray {
+    //越界
+    NSMutableArray *mArray = [NSMutableArray array];
+    [mArray objectAtIndex:2];
+    mArray[2];
+    [mArray insertObject:@"wsl" atIndex:1];
+    [mArray removeObjectAtIndex:3];
+    //nil值
+    [mArray insertObject:nil atIndex:3];
 }
 
 #pragma mark - EventsHandle
