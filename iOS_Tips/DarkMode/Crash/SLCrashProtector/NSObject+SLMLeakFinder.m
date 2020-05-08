eb78c1d362d028492227810aef0c86c49d923ff2
wsl <15324956576@163.com>
2020-05-08 14:59:02 +0800

    内存泄漏

--- iOS_Tips/DarkMode/Crash/SLCrashProtector/NSObject+SLMLeakFinder.m
+++ iOS_Tips/DarkMode/Crash/SLCrashProtector/NSObject+SLMLeakFinder.m
@@ -10,10 +10,10 @@
 
 @implementation NSObject (SLMLeakFinder)
 
-//对象即将释放时调用此方法，定义一个3秒后执行的block，如果正常释放了，weakSelf为nil，不执行notDealloc，否则如果调用了notDealloc，就表示没有释放，出现了内存泄漏。
+//对象即将释放时调用此方法，定义一个2秒后执行的block，如果正常释放了，weakSelf为nil，不执行notDealloc，否则如果调用了notDealloc，就表示没有释放，出现了内存泄漏。
 - (BOOL)willDealloc {
     __weak id weakSelf = self;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [weakSelf notDealloc];
     });
     return YES;
