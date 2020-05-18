754f02015a944074af1f9665c014d67924828e98
wsl <15324956576@163.com>
2020-05-18 19:07:14 +0800

    记录线程的调用栈

---
+++ iOS_Tips/DarkMode/ThirdLibary/BSBacktraceLogger/BSBacktraceLogger.h
@@ -0,0 +1,24 @@
+//
+//  BSBacktraceLogger.h
+//  BSBacktraceLogger
+//
+//  Created by 张星宇 on 16/8/27.
+//  Copyright © 2016年 bestswifter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define BSLOG NSLog(@"%@",[BSBacktraceLogger bs_backtraceOfCurrentThread]);
+#define BSLOG_MAIN NSLog(@"%@",[BSBacktraceLogger bs_backtraceOfMainThread]);
+#define BSLOG_ALL NSLog(@"%@",[BSBacktraceLogger bs_backtraceOfAllThread]);
+
+///记录线程的调用栈    https://github.com/bestswifter/BSBacktraceLogger
+@interface BSBacktraceLogger : NSObject
+
+///返回调用栈字符串 上传或其他处理
++ (NSString *)bs_backtraceOfAllThread;
++ (NSString *)bs_backtraceOfCurrentThread;
++ (NSString *)bs_backtraceOfMainThread;
++ (NSString *)bs_backtraceOfNSThread:(NSThread *)thread;
+
+@end
