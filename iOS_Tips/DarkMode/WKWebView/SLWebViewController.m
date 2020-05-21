eb47f38af922a2cc1577cb9dcc22321b81c6a137
wsl <15324956576@163.com>
2020-05-21 19:06:23 +0800

    WKWebView优化

--- iOS_Tips/DarkMode/WKWebView/SLWebViewController.m
+++ iOS_Tips/DarkMode/WKWebView/SLWebViewController.m
@@ -12,6 +12,8 @@
 @interface SLWebViewController ()
 
 @property (nonatomic, strong) WKWebView * webView;
+///网页加载进度视图
+@property (nonatomic, strong) UIProgressView * progressView;
 
 @end
 
@@ -27,20 +29,29 @@
 - (void)setupUI {
     self.view.backgroundColor = UIColor.whiteColor;
     [self.view addSubview:self.webView];
+    [self.navigationController.navigationBar addSubview:self.progressView];
     
     UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithTitle:@"上一步" style:UIBarButtonItemStyleDone target:self action:@selector(goBackAction:)];
     UIBarButtonItem *forwardItem = [[UIBarButtonItem alloc] initWithTitle:@"下一步" style:UIBarButtonItemStyleDone target:self action:@selector(goForwardAction:)];
     self.navigationItem.rightBarButtonItems = @[forwardItem,backItem];
     
+    //监听网页加载进度
+    [self.webView addObserver:self
+                   forKeyPath:NSStringFromSelector(@selector(estimatedProgress))
+                      options:NSKeyValueObservingOptionNew
+                      context:nil];
 }
 - (void)dealloc {
+    //移除观察者
+    [_webView removeObserver:self
+                  forKeyPath:NSStringFromSelector(@selector(estimatedProgress))];
+    [self.progressView removeFromSuperview];
     NSLog(@"%@释放了",NSStringFromClass(self.class));
 }
 
 #pragma mark - Getter
 - (WKWebView *)webView {
     if(_webView == nil){
-        
         //创建网页配置
         WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
         
@@ -51,8 +62,38 @@
     }
     return _webView;
 }
+- (UIProgressView *)progressView {
+    if (!_progressView){
+        _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 0, SL_kScreenWidth, 2)];
+        _progressView.tintColor = [UIColor blueColor];
+        _progressView.trackTintColor = [UIColor clearColor];
+    }
+    return _progressView;
+}
 
-#pragma mark - Help Methods
+#pragma mark - KVO
+//kvo 监听进度 必须实现此方法
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                      context:(void *)context{
+    
+    if ([keyPath isEqualToString:NSStringFromSelector(@selector(estimatedProgress))]
+        && object == _webView) {
+        //        NSLog(@"网页加载进度 = %f",_webView.estimatedProgress);
+        self.progressView.progress = _webView.estimatedProgress;
+        if (_webView.estimatedProgress >= 1.0f) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progressView.progress = 0;
+            });
+        }
+    }else{
+        [super observeValueForKeyPath:keyPath
+                             ofObject:object
+                               change:change
+                              context:context];
+    }
+}
 
 #pragma mark - Events Handle
 //返回上一步
