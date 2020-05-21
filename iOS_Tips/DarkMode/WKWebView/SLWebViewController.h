b02aec646b5940bded16ae182a2f127526f4ea05
wsl <15324956576@163.com>
2020-05-21 16:08:51 +0800

    WKWebView优化

---
+++ iOS_Tips/DarkMode/WKWebView/SLWebViewController.h
@@ -0,0 +1,17 @@
+//
+//  SLWebViewController.h
+//  DarkMode
+//
+//  Created by wsl on 2020/5/21.
+//  Copyright © 2020 https://github.com/wsl2ls   ----- . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLWebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Tips/DarkMode/WKWebView/SLWebViewController.m
@@ -0,0 +1,24 @@
+//
+//  SLWebViewController.m
+//  DarkMode
+//
+//  Created by wsl on 2020/5/21.
+//  Copyright © 2020 https://github.com/wsl2ls   ----- . All rights reserved.
+//
+
+#import "SLWebViewController.h"
+
+@interface SLWebViewController ()
+
+@end
+
+@implementation SLWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+
+
+@end
