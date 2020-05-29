61dd83afac2bee298ea1023d9f3581b9e499cc55
wsl <15324956576@163.com>
2020-05-29 18:55:13 +0800

    WKWebView缓存

---
+++ iOS_Tips/DarkMode/WKWebView/SLWebCacheViewController.h
@@ -0,0 +1,18 @@
+//
+//  SLWebCacheViewController.h
+//  DarkMode
+//
+//  Created by wsl on 2020/5/29.
+//  Copyright © 2020 https://github.com/wsl2ls   ----- . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// WKWebView 缓存实现
+@interface SLWebCacheViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
