94789217d88876e94a8ffe70998c659510d62147
wsl <15324956576@163.com>
2020-06-09 18:36:17 +0800

    文件目录调整

---
+++ iOS_Tips/DarkMode/AVFoundation/SLAVListViewController.h
@@ -0,0 +1,17 @@
+//
+//  SLAVListViewController.h
+//  DarkMode
+//
+//  Created by wsl on 2020/6/9.
+//  Copyright © 2020 https://github.com/wsl2ls   ----- . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLAVListViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Tips/DarkMode/AVFoundation/SLAVListViewController.m
@@ -0,0 +1,104 @@
+//
+//  SLAVListTableViewController.m
+//  DarkMode
+//
+//  Created by wsl on 2020/6/9.
+//  Copyright © 2020 https://github.com/wsl2ls   ----- . All rights reserved.
+//
+
+#import "SLAVListViewController.h"
+
+#import "SLShotViewController.h"
+#import "SLFaceDetectController.h"
+#import "SLFilterViewController.h"
+#import "SLGPUImageController.h"
+
+@interface SLAVListViewController ()
+@property (nonatomic, strong) NSMutableArray *dataSource;
+@property (nonatomic, strong) NSMutableArray *classArray;
+@end
+
+@implementation SLAVListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupUI];
+    [self getData];
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.hidden = NO;
+}
+- (BOOL)prefersStatusBarHidden {
+    return NO;
+}
+
+#pragma mark - UI
+- (void)setupUI {
+    self.navigationItem.title = @"iOS Tips";
+    self.navigationController.navigationBar.translucent = YES;
+    self.tableView.estimatedRowHeight = 1;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cellID"];
+}
+
+#pragma mark - Data
+- (void)getData {
+    [self.dataSource addObjectsFromArray:@[
+        @"AVFoundation 高仿微信相机拍摄和编辑功能",
+        @"AVFoundation 人脸检测",
+        @"AVFoundation 实时滤镜拍摄和导出",
+        @"GPUImage框架的使用",
+        @"VideoToolBox和AudioToolBox音视频编解码"]];
+    [self.classArray addObjectsFromArray:@[[SLShotViewController class],
+                                           [SLFaceDetectController class],
+                                           [SLFilterViewController class],
+                                           [SLGPUImageController class],
+                                           [UIViewController class]]];
+    [self.tableView reloadData];
+}
+
+#pragma mark - Getter
+- (NSMutableArray *)dataSource {
+    if (_dataSource == nil) {
+        _dataSource = [NSMutableArray array];
+    }
+    return _dataSource;
+}
+- (NSMutableArray *)classArray {
+    if (_classArray == nil) {
+        _classArray = [NSMutableArray array];
+    }
+    return _classArray;
+}
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataSource.count;
+}
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cellID" forIndexPath:indexPath];
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.text =  [NSString stringWithFormat:@"%ld、%@",(long)indexPath.row + 1,self.dataSource[indexPath.row]];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    UIViewController *nextVc = [[self.classArray[indexPath.row] alloc] init];
+    switch (indexPath.row) {
+        case 0:
+        case 1:
+        case 2:
+        case 3: {
+            nextVc.modalPresentationStyle = UIModalPresentationFullScreen;
+            [self presentViewController:nextVc animated:YES completion:nil];
+        }
+            break;
+        case 4:
+            [SLAlertView showAlertViewWithText:@"查看本仓库下的VideoEncoder&Decoder" delayHid:2];
+            NSLog(@"查看本仓库下的VideoEncoder&Decoder");
+            break;
+        default:
+            break;
+    }
+}
+@end
