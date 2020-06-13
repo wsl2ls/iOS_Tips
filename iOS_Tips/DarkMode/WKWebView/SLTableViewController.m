e944eda5bf806f0e8f816bfa7c071dacb675ceb7
wsl <15324956576@163.com>
2020-06-14 00:25:36 +0800

    UITableView的原理

--- iOS_Tips/DarkMode/WKWebView/SLTableViewController.m
+++ iOS_Tips/DarkMode/WKWebView/SLTableViewController.m
@@ -19,6 +19,12 @@
 /// 每一行的坐标位置
 @property (nonatomic, strong) NSMutableArray <NSValue *>*frameArray;
 
+///记录最后一次的偏移量，用来判断滑动方向
+@property (nonatomic, assign) CGFloat lastContentOffsetY;
+///上边即将展示的索引
+@property (nonatomic, assign) NSInteger willDisplayIndexUp;
+///下边即将展示的索引
+@property (nonatomic, assign) NSInteger willDisplayIndexDown;
 
 @end
 
@@ -65,14 +71,28 @@
     return _frameArray;
 }
 
+#pragma mark - Setter
+- (void)setWillDisplayIndexUp:(NSInteger)willDisplayIndexUp{
+    _willDisplayIndexUp = willDisplayIndexUp;
+    _willDisplayIndexUp >= 0 ? _willDisplayIndexUp : -1;
+}
+- (void)setWillDisplayIndexDown:(NSInteger)willDisplayIndexDown {
+    NSInteger count = [self numberOfRowsInScrollView:self.scrollView];
+    _willDisplayIndexDown = willDisplayIndexDown;
+    _willDisplayIndexDown < count ? _willDisplayIndexDown : count;
+}
+
+
 #pragma mark - Help Methods
 ///刷新数据
 - (void)reloadData {
     //清空布局信息
     [self.frameArray removeAllObjects];
+    self.willDisplayIndexUp = -1;
+    self.willDisplayIndexDown = 0;
+    
     //数据源个数
     NSInteger count = [self numberOfRowsInScrollView:self.scrollView];
-    
     CGFloat y = 0;
     //获取每一行的布局信息
     for (int i = 0; i < count; i++) {
@@ -80,11 +100,19 @@
         CGRect rect = CGRectMake(0, y, self.scrollView.sl_width, cellHeight);
         [self.frameArray addObject:[NSValue valueWithCGRect:rect]];
         
+        if (rect.origin.y + rect.size.height < self.scrollView.contentOffset.y) {
+            self.willDisplayIndexUp = i;
+        }
+        
         //按需加载 只加载坐标位置是在当前窗口显示的视图
         if (rect.origin.y + rect.size.height >= self.scrollView.contentOffset.y && rect.origin.y <= self.scrollView.contentOffset.y + self.scrollView.sl_height) {
             UIView *view = [self scrollView:self.scrollView cellForRowAtIndex:i];
             view.frame = rect;
             [self.scrollView addSubview:view];
+        }
+        
+        if (rect.origin.y > self.scrollView.contentOffset.y + self.scrollView.sl_height && self.willDisplayIndexDown == 0) {
+            self.willDisplayIndexDown = i;
         }
         
         //下一行的起始纵坐标
@@ -96,7 +124,6 @@
         }
     }
     
-    //    NSArray *aa = [self indexForVisibleRows];
 }
 //当前可见的cell
 - (NSArray *)visibleCells {
@@ -113,15 +140,39 @@
     }
     return indexs;
 }
-//即将显示cell
-- (void)willDisplayCell{
-    
-    
-    
+//即将显示cell  YES上  NO下
+- (void)willDisplayCellWithDirection:(BOOL)up {
+    if(up) {
+        if (_willDisplayIndexUp < 0) return;
+        NSLog(@"上");
+        CGRect rect = [self.frameArray[self.willDisplayIndexUp] CGRectValue];
+        //按需加载 只加载坐标位置是在当前窗口显示的视图
+        if (rect.origin.y + rect.size.height >= self.scrollView.contentOffset.y && rect.origin.y <= self.scrollView.contentOffset.y + self.scrollView.sl_height) {
+            UIView *view = [self scrollView:self.scrollView cellForRowAtIndex:self.willDisplayIndexUp];
+            view.frame = rect;
+            [self.scrollView addSubview:view];
+            self.willDisplayIndexUp -=1;
+        }
+    }else {
+        NSInteger count = [self numberOfRowsInScrollView:self.scrollView];
+        if (_willDisplayIndexDown == count) return;
+        NSLog(@"下");
+        CGRect rect = [self.frameArray[self.willDisplayIndexDown] CGRectValue];
+        //按需加载 只加载坐标位置是在当前窗口显示的视图
+        if (rect.origin.y + rect.size.height >= self.scrollView.contentOffset.y && rect.origin.y <= self.scrollView.contentOffset.y + self.scrollView.sl_height) {
+            UIView *view = [self scrollView:self.scrollView cellForRowAtIndex:self.willDisplayIndexDown];
+            view.frame = rect;
+            [self.scrollView addSubview:view];
+            self.willDisplayIndexDown +=1;
+        }
+    }
 }
 
 ///根据cellID去复用池reusablePool取可重用的view，如果没有，返回nil
 - (UIView *)dequeueReusableCellWithIdentifier:(nonnull NSString *)cellID {
+    
+    
+    
     return nil;
 }
 ///注册样式
@@ -130,7 +181,7 @@
 }
 ///行数
 - (NSInteger)numberOfRowsInScrollView:(UIScrollView *)scrollView {
-    return 20;
+    return 30;
 }
 ///行高
 - (CGFloat)scrollView:(UIScrollView *)scrollView heightForRowAtIndex:(NSInteger)index {
@@ -150,12 +201,12 @@
 
 #pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    
-    
-    
-    
-    
-    
+    if(scrollView.contentOffset.y > self.lastContentOffsetY) {
+        [self willDisplayCellWithDirection:NO];
+    }else {
+        [self willDisplayCellWithDirection:YES];
+    }
+    self.lastContentOffsetY = scrollView.contentOffset.y;
 }
 
 @end
